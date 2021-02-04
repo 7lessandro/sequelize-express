efbdcccbeb59a31687d745a3ad26e582775c9ded
4scencio <alessandroascencio@protonmail.com>
2021-02-04 14:25:07 -0800

    model user

--- app/models/User.js
+++ app/models/User.js
@@ -1,9 +1,9 @@
 module.exports = (sequelize, Sequelize) => {
-    const User = sequelize.define('User', {
-      name: Sequelize.STRING,
-      email: Sequelize.STRING,
-      password: Sequelize.STRING,
-    });
-  
-    return User;
-  }
+  const User = sequelize.define('User', {
+    name: Sequelize.STRING,
+    email: Sequelize.STRING,
+    password: Sequelize.STRING,
+  });
+
+  return User;
+}
