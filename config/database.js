567756fc32cdd443cff5803d5abc06a553b47e4e
4scencio <alessandroascencio@protonmail.com>
2021-02-04 14:17:58 -0800

    database connection

--- config/database.js
+++ config/database.js
@@ -1,8 +1,19 @@
-module.exports = {
-  username: 'root',
-  password: '123456',
-  database: 'sequelize_express',
-  host: '127.0.0.1',
-  dialect: 'mysql',
-  logging: false
-}
+// module.exports = {
+//   username: 'root',
+//   password: '123456',
+//   database: 'sequelize_express',
+//   host: '127.0.0.1',
+//   dialect: 'mysql',
+//   logging: false
+// }
+
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize('sequelize_express', 'root', '123456', {
+    logging: false,
+    host: 'localhost',
+    dialect: 'mysql'
+    //timezone: '-03:00'
+})
+
+module.exports = connection;
