0655c3ebd77f0302f8b359966cefea6d63ad1ddb
4scencio <alessandroascencio@protonmail.com>
2021-02-04 14:17:41 -0800

    database log connect

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = require('./routes')
 
+const connection = require("./config/database");
+
 const PORT = 8080
 const app = express()
 
@@ -8,6 +10,12 @@ app.use(express.urlencoded({ extended:false }))
 app.use(express.json())
 
 app.use('/', router)
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log('Database OK 📦')
+  })
 
 app.listen(PORT, () => {
     console.log('Server OK, and apparently no problems 😁 ... fow now 👀')
