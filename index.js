c0dca7c0a8513653f40d59844563404ddef7a148
4scencio <alessandroascencio@protonmail.com>
2021-02-04 13:46:32 -0800

    routes

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express')
+const router = require('./routes')
+
 const PORT = 8080
 const app = express()
 
 app.use(express.urlencoded({ extended:false }))
 app.use(express.json())
 
-app.get('/', (request, response) => {
-    console.log('something')
-    response.end()
-})
+app.use('/', router)
 
 app.listen(PORT, () => {
     console.log('Server OK, and apparently no problems 😁 ... fow now 👀')
---
+++ routes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router();
+
+//CONTROLLERS
+
+//MIDDLEWARES
+
+
+
+
+
+/*
+-> ROTAS DA API <-
+*/
+
+//GET
+router.get('/users', (req, res) => {
+    console.log('Olá')
+})
+
+//POST
+
+//PUT
+
+//DELETE
+
+module.exports = router;
