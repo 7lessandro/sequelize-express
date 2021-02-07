5e1cd1a87ef54fede45b4f29ac043021b86c273b
4scencio <alessandroascencio@protonmail.com>
2021-02-07 14:16:24 -0800

    read, read by id and create

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,15 +1,34 @@
 const { User } = require('../app/models');
 
 module.exports = {
-    async index(req, res) {
-        const users = await User.findAll()
-        return res.json(users)
+    async index(request, response) { //READ
+        const users = await User.findAll({attributes: ['id', 'name', 'email', 'createdAt']})
+        return response.json(users)
 
     },
 
-    async store(req, res) {
+    async store(request, response) { //CREATE
         const { name, email, password } = req.body
          const user = await User.create({name, email, password})
-         res.json(user)
+         response.json(user)
+    },
+
+    async show(request, response) { //READ BY ID
+        const { id } = request.params
+        const user = await User.findOne({ where: {id: id}, attributes: ['id', 'name', 'email']})
+
+        if(!user) {
+            response.json({})
+        } else {
+            response.json(user)
+        }
+    },
+
+    async update(request, response) { //UPDATE
+
+    },
+
+    async destroy(request, response) { //DELETE
+        
     }
 }
--- routes.js
+++ routes.js
@@ -5,14 +5,21 @@ const UserController = require('./controllers/UserController')
 
 const routes = express.Router();
 
-
-
 /*
 -> ROTAS DA API <-
 */
 
 //GET
 routes.get('/users', UserController.index)
+routes.get('/user/:id', UserController.show)
+
+//POST
 routes.post('/users', UserController.store)
+
+//PUT
+routes.post('/user/:id', UserController.update)
+
+//DELETE
+routes.post('/user/:id', UserController.destroy)
 
 module.exports = routes;
